dacab7cb0653745388c0986d43ce991cab983a83
Jorge Soto <jls@jorgeluissoto.com>
2023-07-02 23:55:07 -0400

    first commit

---
+++ create_db_tables.sql
@@ -0,0 +1,13 @@
+show databases;
+
+-- Creating databases
+CREATE DATABASE pet_shop;
+CREATE DATABASE SlimeStore;
+
+-- Drop databases
+DROP DATABASE pet_shop;
+
+-- Use and selecting databases
+USE SlimeStore;
+
+-- Tables: Hold the data "a collection of related data held in a sturucture format"
